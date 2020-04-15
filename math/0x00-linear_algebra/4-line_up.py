544eb3d969c99680cfe506314b7bf5595e677a34
dalexach <dalexach@gmail.com>
2020-04-15 21:17:45 +0000

    Adding two arrays

--- math/0x00-linear_algebra/4-line_up.py
+++ math/0x00-linear_algebra/4-line_up.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+"""
+This file contains add_arrays function
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+    Function that add two arrays elemet-wise
+
+    Parameters:
+    - arr1 (list of ints/floats): first list
+    - arr2 (list of ints/floats): second list
+
+    Return:
+     A new list with the first list added second list,
+     if the shape of the lists are not the same, return None
+    """
+
+    # comparing the shape
+    if len(arr1) != len(arr2):
+        return None
+
+    # the new list for the add of the lists
+    add = []
+    # cycle for take number by number
+    for i in range(len(arr1)):
+        add.append(arr1[i] + arr2[i])
+
+    return add
