19eab2c8feb9a65758ffcfbba9fa09bdc7f47bc7
dalexach <dalexach@gmail.com>
2020-04-15 20:46:29 +0000

    Finding the shape of a matrix

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""
+This file contains matrix_shape function
+"""
+
+def matrix_shape(matrix):
+    """
+    Function that receives a matrix and calculate the shape of it
+
+    Parameters:
+     - matrix (list of lists): matrix to calculate the shape
+
+    Return:
+     The shape of the matrix as a list of integers
+    """
+    shape = []
+    while type(matrix) == list:
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
