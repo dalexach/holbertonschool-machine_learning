a96175e1a36b8688b2e949d6d9c6a918544bde19
dalexach <dalexach@gmail.com>
2020-04-16 20:02:45 +0000

    Multiple operations with two matrices

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 """
-This file contains  function
+This file contains np_elementwise(mat1, mat2) function
 """
 
 
-def :
+def np_elementwise(mat1, mat2):
     """
-    Function that
+    Function that performs element-wise addition, subtraction, multiplication,
+    and division
 
     Parameters:
-    - 
+    - mat1 (numpy.ndarray)
+    - mat2 (numpy.ndarray)
 
     Return:
-
+     A tuple containing the element-wise sum, difference, product,
+     and quotient, respectively
     """
+    add = mat1 + mat2
+    diff = mat1 - mat2
+    prod = mat1 * mat2
+    quot = mat1 / mat2
+    return add, diff, prod, quot
