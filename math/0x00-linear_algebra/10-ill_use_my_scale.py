0ed2e2d7a94e89c88af0c43d9bd09cbcdb58edbf
dalexach <dalexach@gmail.com>
2020-04-16 19:50:42 +0000

    Calculating the shape of anumpy.ndarray

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """
-This file contains  function
+This file contains np_shape function
 """
 
 
-def :
+def np_shape(matrix):
     """
-    Function that
+    Function that calculates the shape of a numpy.ndarray
 
     Parameters:
-    - 
+    - matrix (numpy.ndarray)
 
     Return:
-
+     The shape of the numpy.ndarray
     """
+
+    return matrix.shape
