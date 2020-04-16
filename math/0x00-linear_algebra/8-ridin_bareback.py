6a54833914d39999dda93ea0b18122b094c650ff
dalexach <dalexach@gmail.com>
2020-04-16 02:33:15 +0000

    Matrix multiplication

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 """
-This file contains  function
+This file contains mat_mul(mat1, mat2) function
 """
 
 
-def :
+def mat_mul(mat1, mat2):
     """
-    Function that
+    Function that performa matrix multiplication
 
     Parameters:
-    - 
+    - mat1 (list of lists of ints/floats): 2D matrix to multiply
+    - mat2 (list of lists of ints/floats): 2D matrix to multiply
 
     Return:
-
+     A new matrix with the product of the matrices,
+     if the two matrices cannot be multiplied, return None
     """
+
+    if len(mat1[0]) != len(mat2):
+        return None
+
+    m_matrix = []
+    for r in range(len(mat1)):
+        row = []
+        for c in range(len(mat2[0])):
+            i = 0
+            for j in range(len(mat1[0])):
+                n = mat1[r][j] * mat2[j][c]
+                i += n
+            row.append(i)
+        m_matrix.append(row)
+
+    return m_matrix
