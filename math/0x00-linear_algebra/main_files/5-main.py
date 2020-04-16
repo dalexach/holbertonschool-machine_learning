7f3c1130774be57a9f1a4ccd1036235507cd0d40
dalexach <dalexach@gmail.com>
2020-04-16 20:30:50 +0000

    All main files to try the tasks

---
+++ math/0x00-linear_algebra/main_files/5-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+add_matrices2D = __import__('5-across_the_planes').add_matrices2D
+
+mat1 = [[1, 2, 3], [4, 5, 6]]
+mat2 = [[7, 8, 9], [10, 11, 12]]
+print(add_matrices2D(mat1, mat2))
+print(mat1)
+print(mat2)
+print(add_matrices2D(mat1, [[1, 2, 3, 7], [4, 5, 6, 8]]))
