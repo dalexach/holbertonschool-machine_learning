cdc061e82d81b0417a73aa2720f22729e850a447
dalexach <dalexach@gmail.com>
2020-04-24 21:46:45 +0000

    Function to integrate a polynimial

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Calculating the integral of a polynomial
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+     Function that find the integral of a polynomial
+    Arguments:
+     - poly(list of integers): polynomial to calculate the derivate
+     - C (int): constant of integration
+    Return:
+     List of coefficients representing the integral of the polynomial
+    """
+
+    if C is None or type(C) not in (int, float):
+        return None
+
+    if poly is None or poly == [] or type(poly) is not list:
+        return None
+
+    if poly == [0]:
+        return [C]
+
+    integrate = [C]
+    i = 0
+
+    while i < len(poly):
+        if type(poly[i]) not in (int, float) or poly[i] is None:
+            return None
+        coef = poly[i] / (i + 1)
+        if coef.is_integer():
+            coef = int(coef)
+        integrate.append(coef)
+        i += 1
+
+    return integrate
