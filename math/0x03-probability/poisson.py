d39fe7acd00aaf9f21ca72e1272a0ffa1fd889b5
dalexach <dalexach@gmail.com>
2020-04-30 20:14:17 +0000

    fixing lambtha is not valid

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -20,16 +20,18 @@ class Poisson:
         - lambtha (int/float): is the expected number of occurences
         in a given time frame
         """
-        self.lambtha = float(lambtha)
         if data is None:
             if lambtha < 0:
                 raise ValueError("lambtha must be a positive value")
+            else:
+                self.lambtha = float(lambtha)
         else:
             if type(data) is not list:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+            else:
+                self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
         """
