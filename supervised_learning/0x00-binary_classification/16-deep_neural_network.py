2ba22df9efe4e3af58372732588489b28c2cc893
dalexach <dalexach@gmail.com>
2020-05-09 18:40:58 -0500

    Corrections on conditionals

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -30,7 +30,9 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        if type(layers) is not list or len(layers) == 1:
+        if type(layers) is not list:
+            raise TypeError("layers must be an integer")
+        if len(layers) == 0:
             raise TypeError("layers must be an integer")
 
         # public intance attributes
@@ -41,7 +43,9 @@ class DeepNeuralNetwork:
         self.weights = {}
 
         for i in range(self.L):
-            if type(layers[i]) is not int or layers[i] <= 0:
+            if type(layers[i]) is not int:
+                raise TypeError("layers must be an integer")
+            if layers[i] <= 0:
                 raise TypeError("layers must be an integer")
 
             wkey = "W{}".format(i + 1)
