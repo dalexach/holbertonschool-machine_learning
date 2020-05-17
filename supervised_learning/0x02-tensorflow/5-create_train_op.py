9a5a61180dea95fcd31f747aba90c3a1f7742b38
dalexach <dalexach@gmail.com>
2020-05-17 08:23:12 +0000

    Training the NN

--- supervised_learning/0x02-tensorflow/5-create_train_op.py
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+Function create_train_op
+"""
+
+
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Function that creates the training operation for the network
+    Arguments:
+     - loss is the loss of the network’s prediction
+     - alpha is the learning rate
+    Returns:
+    An operation that trains the network using gradient descent
+    """
+    optimizer = tf.train.GradientDescentOptimizer(alpha).minimize(loss)
+
+    return optimizer
