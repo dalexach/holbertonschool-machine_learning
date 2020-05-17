556e17be1dd8bae44ab5ffff91aa6fbe6fe4bb9f
dalexach <dalexach@gmail.com>
2020-05-17 07:55:40 +0000

    Creating layers

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+"""
+Function create_layer
+"""
+
+
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Function create_layer
+    Arguments:
+     - prev: is the tensor output of the previous layer
+     - n: is the number of nodes in the layer to create
+     - activation: is the activation function that the layer should use
+    Returns:
+    The tensor output of the layer
+    """
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=init, name='layer')
+
+    return layer(prev)
