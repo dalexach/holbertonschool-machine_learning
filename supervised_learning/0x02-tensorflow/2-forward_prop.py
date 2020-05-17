744fc68408557fd8cd75a9a67ce7605bbabb0ca6
dalexach <dalexach@gmail.com>
2020-05-17 07:58:57 +0000

    Correction on the loop

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -23,7 +23,7 @@ def forward_prop(x, layer_sizes=[], activations=[]):
     The prediction of the network in tensor form
     """
     layer = create_layer(x, layer_sizes[0], activations[0])
-    for i in range(2, len(layer_sizes)):
+    for i in range(1, len(layer_sizes)):
         layer = create_layer(layer, layer_sizes[i], activations[i])
 
     return layer
