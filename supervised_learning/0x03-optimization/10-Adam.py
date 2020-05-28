ad854acd5b3415598331d67576900d2bdc540c59
dalexach <dalexach@gmail.com>
2020-05-28 22:57:49 +0000

    Adam algorithm upgraded

--- supervised_learning/0x03-optimization/10-Adam.py
+++ supervised_learning/0x03-optimization/10-Adam.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
 """
+Adam optimizer algorithm updated
 """
 
 
-import
+import tensorflow as tf
 
 
-def:
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """
+    Function that creates the training operation for a NN in tensorflow
+    using the Adam optimization algorithm
+    Arguments:
+     - loss is the loss of the network
+     - alpha is the learning rate
+     - beta1 is the weight used for the first moment
+     - beta2 is the weight used for the second moment
+     - epsilon is a small number to avoid division by zero
+    Returns:
+    The Adam optimization operation
     """
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1,
+                                       beta2=beta2, epsilon=epsilon)
+    train = optimizer.minimize(loss)
+
+    return train
