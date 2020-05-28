130cd6f8c9d5a6d1605035fc7b8064b2b50d95cc
dalexach <dalexach@gmail.com>
2020-05-28 22:46:08 +0000

    RMSProp ugraded

--- supervised_learning/0x03-optimization/8-RMSProp.py
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 """
+RMSProp ugraded
 """
 
 
-import
+import tensorflow as tf
 
 
-def:
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """
+    Function that creates the training operation for a NN in tensorflow
+    using the RMSProp optimization algorithm
+    Arguments:
+     - loss is the loss of the network
+     - alpha is the learning rate
+     - beta2 is the RMSProp weight
+     - epsilon is a small number to avoid division by zero
+    Returns:
+     The RMSProp optimization operation
     """
+
+    optimizer = tf.train.RMSPropOptimizer(alpha, epsilon=epsilon, decay=beta2)
+    train = optimizer.minimize(loss)
+
+    return train
