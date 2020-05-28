8b92b00d8c2f978df7a2edbd9a2c7382e457be21
dalexach <dalexach@gmail.com>
2020-05-28 23:11:38 +0000

    Batch normalization

--- supervised_learning/0x03-optimization/13-batch_norm.py
+++ supervised_learning/0x03-optimization/13-batch_norm.py
@@ -1,11 +1,33 @@
 #!/usr/bin/env python3
 """
+Batch Normalization
 """
 
 
-import
+import numpy as np
 
 
-def:
+def batch_norm(Z, gamma, beta, epsilon):
     """
+    Function that normalizes an unactivated output of a NN using
+    batch normalization
+    Arguments:
+     - Z is a numpy.ndarray of shape (m, n) that should be normalized
+         * m is the number of data points
+         * n is the number of features in Z
+     - gamma is a numpy.ndarray of shape (1, n) containing the scales
+        used for batch normalization
+     - beta is a numpy.ndarray of shape (1, n) containing the offsets
+        used for batch normalization
+     - epsilon is a small number used to avoid division by zero
+    Returns:
+    The normalized Z matrix
     """
+
+    m = Z.mean(0)
+    v = Z.var(0)
+
+    Z_n = (Z - m) / (v + epsilon) ** (1/2)
+    NZ = gamma * Z_n + beta
+
+    return NZ
