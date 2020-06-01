97f62fca8a88b3ecb1ee81bc832156a9888621d3
dalexach <dalexach@gmail.com>
2020-06-01 21:44:22 +0000

    Setting up Adam oprimization for a Keras model

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""
+Optimize model
+"""
+
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Function that sets up Adam optimization for a keras model with
+    categorical crossentropy loss and accuracy metrics
+    Arguments:
+     - network is the model to optimize
+     - alpha is the learning rate
+     - beta1 is the first Adam optimization parameter
+     - beta2 is the second Adam optimization parameter
+    Returns:
+     None
+    """
+    adam_optimizer = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(optimizer=adam_optimizer, loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+    return None
