ffdfa3100b783847389c7017747745d67fd38ce8
dalexach <dalexach@gmail.com>
2020-06-01 21:34:50 +0000

    Building a NN with Keras and Sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+"""
+Building a model with Keras
+"""
+
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+    Function that that builds a neural network with the Keras library
+    Arguments:
+     - nx is the number of input features to the network
+     - layers is a list containing the number of nodes in each layer
+        of the network
+     - activations is a list containing the activation functions used for
+        each layer of the network
+     - lambtha is the L2 regularization parameter
+     - keep_prob is the probability that a node will be kept for dropout
+    Returns:
+     The keras model
+    """
+    model = K.Sequential()
+    regularizer = K.regularizers.l2(lambtha)
+    model.add(K.layers.Dense(layers[0], input_shape=(nx,),
+                             activation=activations[0],
+                             kernel_regularizer=regularizer,
+                             name='dense'))
+
+    for layer in range(1, len(layers)):
+        dname = 'dense_' + str(layer)
+        model.add(K.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dense(layers[layer],
+                                 activation=activations[layer],
+                                 kernel_regularizer=regularizer,
+                                 name=dname))
+
+    return model
