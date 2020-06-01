47d994fbca90f3974ffdd7891a525c7f488b47b9
dalexach <dalexach@gmail.com>
2020-06-01 21:52:14 +0000

    Crating a one-hot matrix with labels and Keras

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""
+One hot with Keras
+"""
+
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Function that converts a label vector into a one-hot matrix
+    Arguments:
+     - labels
+     - classes is the number of classes
+    Returns:
+     The one-hot matrix
+    """
+    onehot = K.utils.to_categorical(labels, classes)
+
+    return onehot
