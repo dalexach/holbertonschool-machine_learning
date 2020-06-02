94e2981b5a0a5b77e43302a5436e0063fcdc6ca8
dalexach <dalexach@gmail.com>
2020-06-02 00:12:55 +0000

    Prediction using NN

--- supervised_learning/0x06-keras/13-predict.py
+++ supervised_learning/0x06-keras/13-predict.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""
+Prediction using NN
+"""
+
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+    Function that makes a prediction using a NN
+
+    Arguments:
+     - network is the network model to make the prediction with
+     - data is the input data to make the prediction with
+     - verbose is a boolean that determines if output should be printed
+        during the prediction process
+
+    Returns:
+     The prediction for the data
+    """
+    predicted = network.predict(x=data, verbose=verbose)
+
+    return predicted
