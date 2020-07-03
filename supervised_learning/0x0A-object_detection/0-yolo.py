66d28e2484134039b17d178e700e32cc41ddaa6e
dalexach <dalexach@gmail.com>
2020-07-03 22:31:03 +0000

    some corrections

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -41,7 +41,7 @@ class Yolo():
         self.model = K.models.load_model(model_path)
 
         with open(classes_path, 'r') as f:
-            self.class_names ) [line.strip() for line in f]
+            self.class_names = [line.strip() for line in f]
 
         self.class_t = class_t
         self.nms_t = nms_t
