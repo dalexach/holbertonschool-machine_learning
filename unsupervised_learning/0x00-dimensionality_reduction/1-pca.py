6a4ca458eeca6dbb40447e80a1baaa39929f9e32
dalexach <dalexach@gmail.com>
2020-07-29 16:26:18 -0500

    Performed PCA function

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
 """
+Function PCA performed
 """
 import numpy as np
 
 
-def :
+def pca(X, ndim):
     """
+    Function that performs PCA on a dataset
+
+    Arguments:
+     - X is a numpy.ndarray of shape (n, d) where:
+        * n is the number of data points
+        * d is the number of dimensions in each point
+     - ndim is the new dimensionality of the transformed X
+
+    Returns:
+     T, a numpy.ndarray of shape (n, ndim) containing
+     the transformed version of X
     """
+
+    X_m = X - np.mean(X, axis=0)
+    u, s, vh = np.linalg.svd(X_m)
+    W = vh[:ndim].T
+    T = np.matmul(X_m, W)
+
+    return T
