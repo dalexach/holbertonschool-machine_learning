d220cff3cd630736b55dc32557cdce5a8d419874
dalexach <dalexach@gmail.com>
2020-07-31 15:35:38 -0500

    Correction in the conditional for x argument

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -30,7 +30,7 @@ def likelihood(x, n, P):
     if not isinstance(n, int) or (n <= 0):
         raise ValueError('n must be a positive integer')
 
-    if not isinstance(x, int) or (x <= 0):
+    if not isinstance(x, int) or (x < 0):
         err = 'x must be an integer that is greater than or equal to 0'
         raise ValueError(err)
 
