d2019862fe6f7880cbf52d3cd3e100232072c463
dalexach <dalexach@gmail.com>
2020-07-31 15:32:41 -0500

    PEP8 Correction

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -2,29 +2,37 @@
 """
 Likelihood
 
-You are conducting a study on a revolutionary cancer drug and are looking to find the probability that a patient who takes this drug will develop severe side effects. During your trials, n patients take the drug and x patients develop severe side effects. You can assume that x follows a binomial distribution.
+You are conducting a study on a revolutionary cancer drug and are looking
+to find the probability that a patient who takes this drug will develop severe
+side effects. During your trials, n patients take the drug and x patients
+develop severe side effects. You can assume that x follows
+a binomial distribution.
 """
 import numpy as np
 
 
 def likelihood(x, n, P):
     """
-    Function that calculates the likelihood of obtaining this data given various hypothetical probabilities of developing severe side effects
+    Function that calculates the likelihood of obtaining this data given
+    various hypothetical probabilities of developing severe side effects
 
     Arguments:
      - x is the number of patients that develop severe side effects
      - n is the total number of patients observed
-     - P is a 1D numpy.ndarray containing the various hypothetical probabilities of developing severe side effects
+     - P is a 1D numpy.ndarray containing the various hypothetical
+        probabilities of developing severe side effects
 
     Returns:
-     A 1D numpy.ndarray containing the likelihood of obtaining the data, x and n, for each probability in P, respectively
+     A 1D numpy.ndarray containing the likelihood of obtaining
+     the data, x and n, for each probability in P, respectively
     """
 
     if not isinstance(n, int) or (n <= 0):
         raise ValueError('n must be a positive integer')
 
     if not isinstance(x, int) or (x <= 0):
-        raise ValueError('x must be an integer that is greater than or equal to 0')
+        txt = 'x must be an integer that is greater than or equal to 0'
+        raise ValueError(txt)
 
     if x > n:
         raise ValueError('x cannot be greater than n')
