ccd5486ae327a8e02e8a52622cb18e45b83beaeb
dalexach <dalexach@gmail.com>
2020-08-06 16:53:55 -0500

    Uploading the function

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -13,14 +13,43 @@ def expectation(X, pi, m, S):
     Arguments:
      - X is a numpy.ndarray of shape (n, d) containing the data set
      - pi is a numpy.ndarray of shape (k,) containing the priors for each
-        cluster
+        iter
      - m is a numpy.ndarray of shape (k, d) containing the centroid means for
-        each cluster
+        each iter
      - S is a numpy.ndarray of shape (k, d, d) containing the covariance
-        matrices for each cluster
+        matrices for each iter
 
     Returns:
      g, l, or None, None on failure
         - g is a numpy.ndarray of shape (k, n) containing the posterior
-        probabilities for each data point in each cluster
+        probabilities for each data point in each iter
     """
+
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(m) is not np.ndarray or len(m.shape) != 2:
+        return None, None
+    if type(S) is not np.ndarray or len(S.shape) != 3:
+        return None, None
+    if type(pi) is not np.ndarray or len(pi.shape) != 1:
+        return None, None
+    if X.shape[1] != S.shape[1] or S.shape[1] != S.shape[2]:
+        return (None, None)
+    if X.shape[1] != m.shape[1] or m.shape[0] != S.shape[0]:
+        return (None, None)
+    if pi.shape[0] != m.shape[0]:
+        return (None, None)
+
+    n, d = X.shape
+    k = S.shape[0]
+    tmp = np.zeros((k, n))
+
+    for i in range(k):
+        P = pdf(X, m[i], S[i])
+        prior = pi[i]
+        tmp[i] = prior * P
+
+    g = tmp / np.sum(tmp, axis=0)
+    likelihood = np.sum(np.log(np.sum(tmp, axis=0)))
+
+    return g, likelihood
