08b81295e83bef2642aba38dff7d356f67882ed5
dalexach <dalexach@gmail.com>
2020-08-06 16:29:15 -0500

    Uploading the function

--- unsupervised_learning/0x01-clustering/6-expectation.py
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -24,3 +24,31 @@ def expectation(X, pi, m, S):
         - g is a numpy.ndarray of shape (k, n) containing the posterior
         probabilities for each data point in each cluster
     """
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+    if X.shape[1] != m.shape[1] or X.shape[1] != S.shape[1]:
+        return None, None
+    if X.shape[1] != S.shape[2]:
+        return None, None
+    if not np.isclose(np.sum(pi), 1):
+        return None, None
+
+    n, d = X.shape
+    k = pi.shape[0]
+    tmp = np.zeros((k, n))
+
+    for i in range(k):
+        P = pdf(X, m[i], S[i])
+        tmp[i] = P * pi[i]
+
+    g = tmp / np.sum(tmp, axis=0)
+    likelihood = np.sum(np.log(np.sum(tmp, axis=0)))
+
+    return g, likelihood
