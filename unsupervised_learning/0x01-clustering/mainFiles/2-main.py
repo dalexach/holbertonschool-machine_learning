d6a5e19770d26a42f74ef4b5d9766e4bccd26f95
dalexach <dalexach@gmail.com>
2020-08-06 18:31:25 -0500

    Some corrections

--- unsupervised_learning/0x01-clustering/mainFiles/2-main.py
+++ unsupervised_learning/0x01-clustering/mainFiles/2-main.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
 
     for k in range(1, 11):
         C, _ = kmeans(X, k)
-        print('Variance with {} clusters: {}'.format(k, variance(X, C).round(5)))
+        print('Variance with {} clusters: {}'.format(k, variance(X, C)))
