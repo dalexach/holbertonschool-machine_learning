b6177e1861778213255fee42f3724a79ddbbd60a
dalexach <dalexach@gmail.com>
2020-08-28 18:31:12 -0500

    Second autoencoder

--- unsupervised_learning/0x04-autoencoders/1-sparse.py
+++ unsupervised_learning/0x04-autoencoders/1-sparse.py
@@ -25,3 +25,38 @@ def sparse(input_dims, hidden_layers, latent_dims, lambtha):
         - decoder is the decoder model
         - auto is the sparse autoencoder model
     """
+    # Encoder
+    iencoder = keras.Input(shape=(input_dims, ))
+
+    output = keras.layers.Dense(hidden_layers[0], activation='relu', )(iencoder)
+
+    for i in range(1, len(hidden_layers)):
+        output = keras.layers.Dense(hidden_layers[i],
+                                    activation='relu')(output)
+    reg = keras.regularizers.l1(lambtha)
+    oencoder = keras.layers.Dense(latent_dims,
+                                  activation='relu',
+                                  activity_regularizer=reg)(output)
+
+    # Decoder
+    idecoder = keras.Input(shape=(latent_dims, ))
+    output2 = keras.layers.Dense(hidden_layers[-1],
+                                 activation='relu')(idecoder)
+
+    for i in range(len(hidden_layers)-2, -1, -1):
+        output2 = keras.layers.Dense(hidden_layers[i],
+                                     activation='relu')(output2)
+    odecoder = keras.layers.Dense(input_dims,
+                                  activation='sigmoid',)(output2)
+
+    encoder = keras.models.Model(inputs=iencoder, outputs=oencoder)
+    decoder = keras.models.Model(inputs=idecoder, outputs=odecoder)
+
+    out_encoder = encoder(iencoder)
+    out_decoder = decoder(out_encoder)
+
+    # Autoencoder
+    auto = keras.models.Model(inputs=iencoder, outputs=out_decoder)
+    auto.compile(optimizer='Adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
