8fac0fba0b4a80f333cefe96f4761138503b2ace
dalexach <dalexach@gmail.com>
2020-09-03 16:41:20 -0500

    Function RNN that performs forward propagation for a simple RNN

--- supervised_learning/0x0D-RNNs/1-rnn.py
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -30,11 +30,11 @@ def rnn(rnn_cell, X, h_0):
     _, h = h_0.shape
 
     Y = []
-    H = np.zeros((t+1, m, h))
+    H = np.zeros((t + 1, m, h))
     H[0, :, :] = h_0
-    for t_step in range(t):
-        h, y = rnn_cell.forward(H[t_step], X[t_step])
-        H[t_step+1, :, :] = h
+    for step in range(t):
+        h, y = rnn_cell.forward(H[step], X[step])
+        H[step + 1, :, :] = h
         Y.append(y)
     Y = np.asarray(Y)
 
