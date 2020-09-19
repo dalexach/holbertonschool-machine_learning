5120fef1b74a6c7415afe1b8f7b2b4dd4e50455f
dalexach <dalexach@gmail.com>
2020-09-18 21:25:57 -0500

    task 0

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python3
+"""
+Unigram BLEU score
+"""
+import numpy as np
+
+
+def uni_bleu(references, sentence):
+    """
+    Function that calculates the unigram BLEU score for a sentence
+
+    Arguments:
+     - references is a list of reference translations
+        * each reference translation is a list of the words in the translation
+     - sentence is a list containing the model proposed sentence
+
+    Returns:
+     The unigram BLEU score
+    """
+
+    len_output = len(sentence)
+    counter = 0
+    len_clipped = 0
+
+    for w in sentence:
+        counter += sentence.count(w)
+        maxi = 0
+        for ref in references:
+            tmp = ref.count(w)
+            if tmp > maxi:
+                maxi = tmp
+        len_clipped += maxi
+
+    closest_idx = np.argmin([abs(len(x) - len_output) for x in references])
+    closest_len_refer = len(references[closest_idx])
+
+    if len_output > closest_len_refer:
+        bp = 1
+    else:
+        bp = np.exp(1 - closest_len_refer / len_output)
+
+    BLEU_score = bp * (len_clipped / counter)
+
+    return BLEU_score
