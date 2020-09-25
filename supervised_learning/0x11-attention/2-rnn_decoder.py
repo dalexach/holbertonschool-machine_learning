8a55692ef7bc7809dcc72bf713d3d7814394cd9b
dalexach <dalexach@gmail.com>
2020-09-24 21:37:10 -0500

    Task 2

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -65,13 +65,13 @@ class RNNDecoder(tf.keras.layers.Layer):
             containing the new decoder hidden state
         """
 
-        x = self.embedding(x)
         attention = SelfAttention(s_prev.shape[1])
         context, weights = attention(s_prev, hidden_states)
+        x = self.embedding(x)
         x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
         decode_outs, state = self.gru(x)
 
-        y = tf.reshape((decode_outs), (-1, decode_outs.shape[2]))
-        y = self.F(y)
+        decode_outs = tf.reshape(decode_outs, (-1, decode_outs.shape[2]))
+        y = self.F(decode_outs)
 
         return y, state
