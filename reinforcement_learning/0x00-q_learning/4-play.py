53eb7c403e6a059bd2da1d2dd7b661b0181f002d
dalexach <dalexach@gmail.com>
2020-09-28 22:06:21 -0500

    Task 4

--- reinforcement_learning/0x00-q_learning/4-play.py
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -2,7 +2,11 @@
 """
 Play
 """
-import gym
+import numpy as np
+epsilon_greedy = __import__('2-epsilon_greedy').epsilon_greedy
+load_frozen_lake = __import__('0-load_env').load_frozen_lake
+q_init = __import__('1-q_init').q_init
+train = __import__('3-q_learning').train
 
 
 def play(env, Q, max_steps=100):
@@ -17,3 +21,22 @@ def play(env, Q, max_steps=100):
     Returns:
      The total rewards for the episode
     """
+
+    state = env.reset()
+    done = False
+    env.render()
+
+    for step in range(max_steps):
+        action = np.argmax(Q[state, :])
+        new_state, reward, done, info = env.step(action)
+        env.render()
+
+        if done is True:
+            if reward == 1:
+                print(reward)
+                break
+        state = new_state
+
+    env.close()
+
+    return reward
