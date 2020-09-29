3abb06ca3eba51882d56792ebab9975b1dc8ae6c
dalexach <dalexach@gmail.com>
2020-09-28 22:05:24 -0500

    Task 0

--- reinforcement_learning/0x00-q_learning/0-load_env.py
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -22,3 +22,8 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     Returns:
      The environment
     """
+
+    env = gym.make("FrozenLake-v0", desc=desc,
+                   map_name=map_name, is_slippery=is_slippery)
+
+    return env
