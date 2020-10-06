eed96a5bbdc7d9eb3c8b18714ed67d381b425409
dalexach <dalexach@gmail.com>
2020-10-05 23:55:01 -0500

    PEP8 correction

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -10,23 +10,25 @@ class Dataset():
     """
     Class that loads and preps a dataset for machine translation
     """
-    
+
     def __init__(self):
         """
         Class constructor
         Instance attributes:
-         - data_train, which contains the ted_hrlr_translate/pt_to_en tf.data.Dataset train split, loaded as_supervided
-         - data_valid, which contains the ted_hrlr_translate/pt_to_en tf.data.Dataset validate split, loaded as_supervided
-         - tokenizer_pt is the Portuguese tokenizer created from the training set
-         - tokenizer_en is the English tokenizer created from the training set
+         - data_train, which contains the ted_hrlr_translate/pt_to_en
+            tf.data.Dataset train split, loaded as_supervided
+         - data_valid, which contains the ted_hrlr_translate/pt_to_en
+            tf.data.Dataset validate split, loaded as_supervided
+         - tokenizer_pt is the Portuguese tokenizer created
+            from the training set
+         - tokenizer_en is the English tokenizer created
+            from the training set
         """
 
         examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
                                        with_info=True,
                                        as_supervised=True)
 
-        """self.data_train, self.data_valid = examples['train'], \
-            examples['validation']"""
         self.data_train = examples['train']
         self.data_valid = examples['validation']
 
@@ -38,7 +40,8 @@ class Dataset():
         Instance method that creates sub-word tokenizers for our dataset
 
         Arguments:
-         - data is a tf.data.Dataset whose examples are formatted as a tuple (pt, en)
+         - data is a tf.data.Dataset whose examples are
+            formatted as a tuple (pt, en)
             * pt is the tf.Tensor containing the Portuguese sentence
             * en is the tf.Tensor containing the corresponding English sentence
 
@@ -50,10 +53,10 @@ class Dataset():
 
         tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
                 (pt.numpy() for pt, en in data),
-                target_vocab_size=2 ** 15)
+                target_vocab_size=2**15)
 
         tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
                 (en.numpy() for pt, en in data),
-                target_vocab_size=2 ** 15)
+                target_vocab_size=2**15)
 
         return tokenizer_pt, tokenizer_en
