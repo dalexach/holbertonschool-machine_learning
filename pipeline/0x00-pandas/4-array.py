0fc41d4bef8bb19d508c456aa1f91f34ed4349b2
dalexach <dalexach@gmail.com>
2020-10-26 23:10:10 -0500

    Task 4 solved

--- pipeline/0x00-pandas/4-array.py
+++ pipeline/0x00-pandas/4-array.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
+"""
+To Numpy
+"""
 
 import pandas as pd
 from_file = __import__('2-from_file').from_file
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
-A = # YOUR CODE HERE
+"""
+Take the last 10 rows of the columns High and Close
+and convert them into a numpy.ndarray
+"""
+A = df.loc[:, ['High', 'Close']].tail(10).to_numpy()
 
 print(A)
