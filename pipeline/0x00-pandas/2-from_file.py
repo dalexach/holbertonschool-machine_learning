a5cfd0d7eb51bc32e4c853d472fc5b1cd8cdc129
dalexach <dalexach@gmail.com>
2020-10-26 22:40:35 -0500

    Task 2 solved

--- pipeline/0x00-pandas/2-from_file.py
+++ pipeline/0x00-pandas/2-from_file.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""
+From File
+"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+    Function that loads data from a file as a pd.DataFrame
+
+    Arguments:
+     - filename is the file to load from
+     - delimiter is the column separator
+
+    Returns:
+     The loaded pd.DataFrame
+    """
+
+    df = pd.read_csv(filename, sep=delimiter)
+
+    return df
