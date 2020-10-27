1c2328ff16ec31a8ee5e69d3a693f36513bcacc1
dalexach <dalexach@gmail.com>
2020-10-26 22:24:01 -0500

    Task 0 solved

--- pipeline/0x00-pandas/0-from_numpy.py
+++ pipeline/0x00-pandas/0-from_numpy.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+import pandas as pd
+"""
+From Numpy
+"""
+
+
+def from_numpy(array):
+    """
+    Function that creates a pd.DataFrame from a np.ndarray
+
+    Arguments:
+     - array is the np.ndarray from which you should create the pd.DataFrame
+
+    Note:
+    The columns of the pd.DataFrame should be labeled in alphabetical order
+     and capitalized.
+    There will not be more than 26 columns.
+
+    Returns:
+     The newly created pd.DataFrame
+    """
+
+    alpha = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+    if array.shape[1] > 26:
+        df = pd.DataFrame(array)
+        df = df.iloc[:, 0:26]
+        df.columns = alpha
+
+    else:
+        col = alpha[:array.shape[1]]
+        df = pd.DataFrame(array, columns=col)
+
+    return df
