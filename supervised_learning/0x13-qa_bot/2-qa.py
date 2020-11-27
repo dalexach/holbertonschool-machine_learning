e2545da37d6d4d8aa62a81afec6e818c87546a4c
dalexach <dalexach@gmail.com>
2020-11-26 23:56:08 -0500

    Uploading the solution for task 2

--- supervised_learning/0x13-qa_bot/2-qa.py
+++ supervised_learning/0x13-qa_bot/2-qa.py
@@ -2,6 +2,7 @@
 """
 Answer Questions
 """
+question_answer = __import__('0-qa').question_answer
 
 
 def answer_loop(reference):
@@ -15,3 +16,19 @@ def answer_loop(reference):
     If the answer cannot be found in the reference text,
     respond with Sorry, I do not understand your question.
     """
+
+    cases = ['exit', 'goodbye', 'bye']
+
+    while True:
+        question = input('Q: ')
+        question = question.lower()
+
+        if question in cases:
+            print('A: Goodbye')
+            exit(0)
+        else:
+            answer = question_answer(question, reference)
+            if answer is None or answer == '':
+                print('A: Sorry, I do not understand your question.')
+            else:
+                print('A: {}'.format(answer))
