b40dc6f9729914cbf38c270e40fa8571485dcfd0
dalexach <dalexach@gmail.com>
2020-11-26 23:55:34 -0500

    Uploading the solution for task 1

--- supervised_learning/0x13-qa_bot/1-loop.py
+++ supervised_learning/0x13-qa_bot/1-loop.py
@@ -6,3 +6,15 @@ and prints A: as a response.
 If the user inputs exit, quit, goodbye, or bye, case insensitive,
 print A: Goodbye and exit
 """
+
+cases = ['exit', 'goodbye', 'bye']
+
+while True:
+    ans = input('Q: ')
+    ans = ans.lower()
+
+    if ans in cases:
+        print('A: Goodbye')
+        exit(0)
+    else:
+        print('A: ')
